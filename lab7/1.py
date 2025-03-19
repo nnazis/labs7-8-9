598cdcfc86a88b75bf3ffb08e3ee7d9af48028f2
Nazira <nazira08.com@gmail.com>
2025-03-19 13:14:55 +0500

    Исправленный код

--- lab7/1.py
+++ lab7/1.py
@@ -1,37 +1,37 @@
 import pygame
 import time
-import math
 pygame.init()
-width, height = 400, 400
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Mickey Clock")
-background = pygame.image.load("clock.png")  
-right_hand = pygame.image.load("rightarm.png")  
-left_hand = pygame.image.load("leftarm.png") 
-background = pygame.transform.scale(background, (width, height))
-right_hand = pygame.transform.scale(right_hand, (20, 150))
-left_hand = pygame.transform.scale(left_hand, (15, 120))
-center = (width // 2, height // 2)
-def rotate_hand(image, angle, offset):
-    rotated_image = pygame.transform.rotate(image, angle)
-    rect = rotated_image.get_rect(center=(center[0] + offset[0], center[1] + offset[1]))
-    return rotated_image, rect
+screen = pygame.display.set_mode((1480, 1090))
+clock = pygame.time.Clock()
+background = pygame.image.load("clock.png")
+min_hand = pygame.image.load("rightarm.png")
+sec_hand = pygame.image.load("leftarm.png")
+s_sec = pygame.transform.scale(sec_hand, (sec_hand.get_width()*0.9, sec_hand.get_height()*0.9))
+s_min = pygame.transform.scale(min_hand, (min_hand.get_width()*1, min_hand.get_height()*1))
+p = (700, 525)
+angle_sec = 0
+angle_min = 0
+def rotate(image, angle, p):
+    r_image = pygame.transform.rotate(image, -angle)
+    r_rect = r_image.get_rect(center=p)
+    return r_image, r_rect
 running = True
 while running:
-    screen.fill((255, 255, 255))
-    screen.blit(background, (0, 0))
-    current_time = time.localtime()
-    minutes = current_time.tm_min
-    seconds = current_time.tm_sec
-    minute_angle = -(minutes * 6) 
-    second_angle = -(seconds * 6)     
-    r_hand, r_rect = rotate_hand(right_hand, minute_angle, (0, -75))
-    l_hand, l_rect = rotate_hand(left_hand, second_angle, (0, -60))    
-    screen.blit(r_hand, r_rect.topleft)
-    screen.blit(l_hand, l_rect.topleft)    
-    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False 
-    pygame.time.delay(1000)  
+            running = False
+    current_time = time.localtime()
+    seconds = time.time() % 60
+    minutes = (time.time() // 60) % 60
+    angle_sec = (seconds*6 - 90)
+    angle_min = ((minutes*6 + seconds * 0.1) - 90)
+    r_sec, r_sec_rect = rotate(s_sec, angle_sec, p)
+    r_min, r_min_rect = rotate(s_min, angle_min, p)
+    screen.fill((255, 255, 255))
+    screen.blit(background, (6, 11))
+    screen.blit(r_sec, r_sec_rect.topleft)
+    screen.blit(r_min, r_min_rect.topleft)
+    pygame.display.flip()
+    clock.tick(60)
+    pygame.time.delay(10)
 pygame.quit()
